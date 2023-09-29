7e828bc16b5a5b507c727fb703aae9c68a919452
ualvarez23 <uriel.az2303@gmail.com>
2023-09-28 21:44:40 -0600

    se agrega el tema variable de entorno que vienen de afuera

---
+++ entorno.js
@@ -0,0 +1,13 @@
+// variables de entorno
+
+// powerShell primero se agrega la variable
+// $env:NOMBRE="URIEL"
+// Luego se llama el proceso
+// node entorno.js
+// Las variables de entorno vienen de afuera
+
+let nombre = process.env.NOMBRE || 'sin comentarios'
+let web = process.env.WEB || 'no tiene web'
+
+console.log("Hola " +nombre);
+console.log("Mi web es:  " +web);
--- monohilo.js
+++ monohilo.js
@@ -1,5 +1,15 @@
 console.log("Hola, mundo");
 
+
+let i = 0;
+
 setInterval(function(){
+//     console.log(i)
+//     i++;
     console.log("Sigo activo")
+//     if(i==5){
+//         var a = 3 + z;
+//     }
 }, 1000)
+
+console.log("segunda instrucciones")
