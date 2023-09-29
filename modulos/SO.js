139ed7ce1aee0742d6b38aef46362f9aa1bf8be7
Uriel Alvarez <uriel.az2303@gmail.com>
2023-09-29 12:48:45 -0600

    se agrega comandos para console log del sistema operativo

---
+++ modulos/SO.js
@@ -0,0 +1,34 @@
+const os = require('os');
+
+// arquitectura de la computadora
+// console.log(os.arch())
+// plataforma de sistema operativo
+// console.log(os.platform())
+
+// core
+// console.log(os.cpus())
+// console.log(os.cpus().length)
+
+// errores del sistema constantes
+// console.log(os.constants)
+
+// bytes libres de memoria
+// console.log(os.freemem())
+
+// const SIZE = 1024
+// function kb(bytes){ return bytes / SIZE}
+// function mg(bytes){ return kb(bytes) / SIZE}
+// function gb(bytes){ return mg(bytes) / SIZE}
+// console.log(kb(os.freemem))
+// console.log(mg(os.freemem))
+// console.log(gb(os.freemem))
+// console.log(gb(os.totalmem)) RAM
+
+
+// console.log(os.totalmem())
+
+// console.log(os.homedir())direccion
+// console.log(os.tmpdir()) temporar
+
+// console.log(os.hostname())
+// console.log(os.networkInterfaces())interfaces de red
