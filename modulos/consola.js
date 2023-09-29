50a0cd77de8750010b615f605c22a9403719ace6
Uriel Alvarez <uriel.az2303@gmail.com>
2023-09-29 11:31:37 -0600

    se agrega ejemplos de consola, console.log etc.

---
+++ modulos/consola.js
@@ -0,0 +1,30 @@
+// console.log("Algo")
+// console.info("Algo")
+// console.error("Algo")
+// console.warn("Algo")
+
+// var tabla=[{
+//     a:1,
+//     b:"Z"
+// },
+// {
+//     a:2,
+//     b:"Otra"
+// },
+// ]
+
+// console.log(tabla)
+// console.table(tabla)  
+
+// console.group("Conversacion:")
+// console.log("hola")
+// console.log("bla bla bla")
+// console.log("adios")
+// console.groupEnd("Conversacion:")
+// console.log("otra Cosa")
+
+console.count("veces")
+console.count("veces")
+console.count("veces")
+console.countReset("veces")
+console.count("veces")
